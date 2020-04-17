ccacdb60e5f2dfdbf588ad9d9a889eb5bd143452
unknown <thetavog@gmail.com>
2020-04-17 16:16:54 -0500

    created project and added identity

---
+++ oSport/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace oSport.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+                .HasData(
+                new IdentityRole
+                {
+                    Name = "League Admin",
+                    NormalizedName = "LEAGUE Admin",
+                },
+                new IdentityRole
+                {
+                    Name = "Coach",
+                    NormalizedName = "COACH",
+                },
+                new IdentityRole
+                {
+                    Name = "Referee",
+                    NormalizedName = "REFEREE",
+                },
+                new IdentityRole
+                {
+                    Name = "Player",
+                    NormalizedName = "PLAYER",
+                }
+                );
+        }
+    }
+}
---
+++ oSport/Data/Migrations/20200417211242_Init.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace oSport.Data.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2df65992-dec6-455c-a9c5-1e1362f37707", "6fcc41e0-b7a0-4244-807c-7a6f6931bc4d", "League Admin", "LEAGUE Admin" },
+                    { "ffd67eb7-c71e-4ed0-bbdd-459f8bde4aef", "e8be01ea-90e1-4f88-89d8-f8c66c7813a1", "Coach", "COACH" },
+                    { "c0b69623-ede8-4d1c-8b9a-c87dda51e9ec", "da6de49d-d07d-44ab-a703-728642a4b3db", "Referee", "REFEREE" },
+                    { "ac7f10ca-3bb0-4a0f-941a-d60c4d7f1dea", "90cda2ee-2981-49bd-b154-d57ce791376f", "Player", "PLAYER" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2df65992-dec6-455c-a9c5-1e1362f37707");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ac7f10ca-3bb0-4a0f-941a-d60c4d7f1dea");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c0b69623-ede8-4d1c-8b9a-c87dda51e9ec");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ffd67eb7-c71e-4ed0-bbdd-459f8bde4aef");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
